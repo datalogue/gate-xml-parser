1956f352629346c601d56cd21f3183ee20f6dfec
Noah Shpak <noahshpak@gmail.com>
2018-12-03 15:08:58 -0500

    clean up and accurate BIO tagging scheme

--- gateparser/parser.py
+++ gateparser/parser.py
@@ -14,23 +14,23 @@ from spacy.tokenizer import Tokenizer
 nlp = spacy.load('en')
 
 
-
 class GateBIOParser(object):
     def __init__(self, filename, language='english',
-                                 encoding='windows-1252',
-                                 annotation_spec='GateDocument.AnnotationSet',
-                                 node_spec='GateDocument.TextWithNodes',
-                                 text_spec='GateDocument.TextWithNodes'):
+                 encoding='windows-1252',
+                 annotation_spec='GateDocument.AnnotationSet',
+                 node_spec='GateDocument.TextWithNodes',
+                 text_spec='GateDocument.TextWithNodes'):
         self.filename = filename
         self._encoding = encoding
         self._annotation_spec = annotation_spec
         self.language = language
         self._node_spec = node_spec
         self._text_spec = text_spec
+
+        nlp.tokenizer = self._create_custom_tokenizer(nlp)
+
         self.annotations, self.nodes, self.text = self.load_xml()
         self.BIO = self._tag_bio()
-
-
 
     def load_xml(self):
         if not '.xml' in self.filename:
@@ -61,7 +61,8 @@ class GateBIOParser(object):
                 annos = parsed_annos = None
 
             if annos is not None and glom(annotations, self._text_spec) is None:
-                raise ValueError('Error in Annotation File. No Annotations, but Nodes are present.')
+                raise ValueError(
+                    'Error in Annotation File. No Annotations, but Nodes are present.')
 
             nodes = glom(annotations, self._node_spec)
 
@@ -70,120 +71,106 @@ class GateBIOParser(object):
 
             if annos is not None and nodes is not None:
                 text = glom(annotations, self._text_spec)['#text']
-
-
             else:
-                # warnings.warn(f'No Text to Annotate in {self.filename}')
                 text = None
-
 
         return annos, nodes, text
 
+    def _consolidate_label(self, label):
+        if label == 'UserIDWindows':
+            return 'UserIDGeneric'
+        elif label in ('LastName', 'FirstName'):
+            return 'FullName'
+        elif label in ('Email', 'JobTitle'):
+            return None
+        return label
+
+    def calc_end_idx(self, token_end_idx, end, token_char_ends):
+        while True:
+            try:
+                if int(end) <= int(token_char_ends[token_end_idx]):
+                    break
+                token_end_idx += 1
+            except IndexError:
+                if int(end) - 1 <= int(token_char_ends[token_end_idx-1]):
+                    token_end_idx -= 1
+                    break
+                else:
+                    raise ValueError('LooP BrokE')
+        else:
+            raise ValueError('-- start found but not end -- ')
+        return token_end_idx
+
+    def _create_custom_tokenizer(self, nlp):
+        custom_prefixes = ['/', '\->']
+        all_prefixes_re = spacy.util.compile_prefix_regex(
+            tuple(list(nlp.Defaults.prefixes) + custom_prefixes))
+
+        custom_infixes = [
+            '\+', '\(', '\&', '\.', '\,', '\)', '\?', '\->', '\-', '\[', '\]']
+        infix_re = spacy.util.compile_infix_regex(
+            tuple(list(nlp.Defaults.infixes) + custom_infixes))
+
+        suffix_re = spacy.util.compile_suffix_regex(nlp.Defaults.suffixes)
+
+        return Tokenizer(nlp.vocab, nlp.Defaults.tokenizer_exceptions,
+                         prefix_search=all_prefixes_re.search,
+                         infix_finditer=infix_re.finditer, suffix_search=suffix_re.search,
+                         token_match=None)
+
+    def extract_tokens(self, text):
+        doc = nlp(text, disable=['ner'])
+        return [str(t) for t in list(doc)]
 
     def _tag_bio(self):
         annos, nodes, text = self.annotations, self.nodes, self.text
         if not nodes or not text:
             return None
         if not annos:
-            sentences, _, _ = self._tokenize_sentences()
-            empty_out = [{'tokens': tokens,'labels': ['O'] * len(tokens)} for tokens in sentences]
+            tokens = self.extract_tokens(text)
+            empty_out = {'tokens': tokens, 'labels': ['O'] * len(tokens)}
             return empty_out
         elif isinstance(annos, list):
            # handle many annos
-            annos = {(a['@StartNode'], a['@EndNode']): a['@Type'] for a in annos}
+            annos = {(a['@StartNode'], a['@EndNode']): a['@Type']
+                     for a in annos}
         else:
-            annos = { (annos['@StartNode'], annos['@EndNode']): annos['@Type'] }
+            annos = {(annos['@StartNode'], annos['@EndNode']): annos['@Type']}
 
         entity_indices = annos.keys()
-
-        def create_custom_tokenizer(nlp):
-            custom_prefixes = ['/', '\->']
-            all_prefixes_re = spacy.util.compile_prefix_regex(tuple(list(nlp.Defaults.prefixes) + custom_prefixes))
-
-            custom_infixes = ['\+', '\(', '\&', '\.', '\,', '\)', '\?', '\->', '\-', '\[', '\]']
-            infix_re = spacy.util.compile_infix_regex(tuple(list(nlp.Defaults.infixes) + custom_infixes))
-
-            suffix_re = spacy.util.compile_suffix_regex(nlp.Defaults.suffixes)
-
-            return Tokenizer(nlp.vocab, nlp.Defaults.tokenizer_exceptions,
-                             prefix_search = all_prefixes_re.search,
-                             infix_finditer = infix_re.finditer, suffix_search = suffix_re.search,
-                             token_match=None)
-
-
-        nlp.tokenizer = create_custom_tokenizer(nlp)
-
+        tokens = self.extract_tokens(text)
+        labels = ['O'] * len(tokens)
         doc = nlp(text, disable=['ner'])
 
-        tokens, labels = [str(t) for t in list(doc)], ['O'] * len(doc)
-        token_char_starts = [str(doc[i:i+1].start_char) for i in range(len(tokens))]
-        token_char_ends = [str(doc[i:i+1].end_char - 1) for i in range(len(tokens))]
+        token_char_starts = [str(doc[i:i+1].start_char)
+                             for i in range(len(tokens))]
+        token_char_ends = [str(doc[i:i+1].end_char - 1)
+                           for i in range(len(tokens))]
 
-        ws = [self.text[int(i):int(j)].strip() for i,j in entity_indices]
+        for (start, end) in entity_indices:
 
-        for entity_index, (start, end) in enumerate(entity_indices):
-            char_start_idx = -1
-
-            if start in token_char_starts:
-                char_start_idx = start
-                adjustment = 0
-            elif str(int(start) - 1) in token_char_starts:
-                char_start_idx = str(int(start) - 1)
-                adjustment = 1
-            elif str(int(start) + 1) in token_char_starts:
-                char_start_idx = str(int(start) + 1)
-                adjustment = 1
-
-            if char_start_idx == -1:
+            if not start in token_char_starts:
                 raise ValueError('Char Start not set')
 
-            token_start_idx = token_char_starts.index(char_start_idx)
-            token_end_idx = token_start_idx
-
-
-            while True:
-                try:
-                    if int(end) <= int(token_char_ends[token_end_idx]):
-                        break
-                    token_end_idx += 1
-                except IndexError as e:
-                    if int(end) - 1  <= int(token_char_ends[token_end_idx-1]):
-                        token_end_idx -= 1
-                        break
-                    else:
-                        raise ValueError('LooP BrokE')
-
-
-            else:
-                raise ValueError('-- start found but not end -- ')
-
+            token_start_idx = token_char_starts.index(start)
+            token_end_idx = self.calc_end_idx(
+                token_start_idx, end, token_char_ends)
 
             if (start, end) not in annos:
                 raise ValueError('Parse Error!')
 
-
-            label = annos[(start, end)]
-
-            if label == 'UserIDWindows':
-                label = 'UserIDGeneric'
-            elif label in ('LastName', 'FirstName'):
-                label = 'FullName'
-            elif label in ('Email', 'JobTitle'):
+            label = self._consolidate_label(annos[(start, end)])
+            if label is None:
                 continue
 
             if token_start_idx < token_end_idx:
                 for i in range(int(token_start_idx), int(token_end_idx)):
-                    # if tokens[i] in ',)(' and label != 'TelNumber:
-                    #     continue
-                    # else:
                     if tokens[i]:
                         labels[i] = 'I-' + label
 
                 # B-scheme
-                if ' ' in ws[entity_index] and label not in ('UserIDGeneric', 'Organisation'):
-                    if int(token_end_idx) - int(token_start_idx) > 1:
-                        if all(labels[i] == 'I-' + label for i in range(int(token_start_idx), int(token_end_idx))):
-                               labels[token_start_idx] = 'B-' + label
+                if 0 < token_start_idx - 1 < len(labels) and labels[token_start_idx - 1] == 'I-' + label:
+                    labels[token_start_idx] = 'B-' + label
 
             elif token_start_idx > token_end_idx:
                 raise ValueError('Indices are incorrect')
@@ -192,13 +179,9 @@ class GateBIOParser(object):
 
         return {'tokens': tokens, 'labels': labels}
 
-
-
     def get_class_counts(self):
-        classes = set(self.BIO['labels'])
         counts = Counter(self.BIO['labels'])
         return counts
-
 
     def print_class_counts(self):
         class_counts = self.get_class_counts()
